0b8e636658cd0c65b97c4c21e94189bd44e85cea
Hari Prasad Paidipati <hariprasad.paidipati@gmail.com>
2020-07-02 17:34:26 +0800

    Compliance Dashboard added

--- src/views/MyDashBoard.js
+++ src/views/MyDashBoard.js
@@ -28,24 +28,89 @@ import {
   Col
 } from "reactstrap";
 import { getMyClientInteractions } from "services/my-client-interactions";
+import { mapStateToProps } from "./TeamDashBoard";
+import { connect } from "react-redux";
+import { getClientInteractions } from "services/my-client-interactions";
 
-class Tables extends React.Component {
+class ComplianceDashboard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-       conversations: [] 
+       chatGroups: [],
+       groupSelected: false,
+       userSelected: false,
+       users: [],
+       unAcceptableContacts: [],
+       selectedUser: '',
+       groupName: ''
+    };
+
+    this.onGroupSelect = this.onGroupSelect.bind(this);
+    this.onUserSelect = this.onUserSelect.bind(this);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.data && prevProps.data !== this.props.data ) {
+       const chatGroups = Object.keys(this.props.data).map(key => {
+         const groupContacts = Object.keys(this.props.data[key]).map(userKey => {
+          this.props.data[key][userKey].totalUnacceptableContacts.map(contact => contact.smpIdentifier)
+        })
+  
+        const unAcceptableContacts = [].concat.apply([], groupContacts);
+
+         return {
+           name: key,
+           region: 'APAC',
+           platform: 'weChat',
+           unAcceptableContacts: unAcceptableContacts.length
+         }
+       });
+
+      this.setState({
+        chatGroups: chatGroups
+      })
     }
   }
 
-  componentDidMount() {
-    
-       const conversations = getMyClientInteractions()
+  onGroupSelect(name) {
 
-       if (conversations) {
-         this.setState({
-          conversations
-         })
-       }
+    const users = [];
+    
+    Object.keys(this.props.data[name]).forEach(key => {
+        users.push({
+            sid: this.props.data[name][key].userDetails.id,
+            name: this.props.data[name][key].userDetails.name,
+            platform: 'WeChat', // hardcoded for now as server does not return it
+            region: 'APAC', // hardcoded for now as server does not return it
+            totalContacts: this.props.data[name][key].totalNumberOfContacts,
+            totalUnacceptableContacts: this.props.data[name][key].totalUnacceptableContacts
+        });
+    });
+
+    console.log(users);
+    this.setState({
+      users,
+      groupSelected: true,
+      userSelected: false,
+      groupName: name
+    });
+  }
+
+  onUserSelect(sid) {
+    if (sid) {
+      console.error('this.props.data[this.groupName]', this.props.data[this.state.groupName]);
+        const teamMemberContacts = getClientInteractions(sid, this.props.data[this.state.groupName]);
+          
+        if (teamMemberContacts) {
+          this.setState({
+            teamMemberSelected: true,
+            selectedUser: `${name} ( ${sid} )`,
+            unAcceptableContacts: teamMemberContacts,
+            userSelected: true
+          });
+        }
+  
+    }
   }
 
   render() {
@@ -62,23 +127,21 @@ class Tables extends React.Component {
                   <Table className="tablesorter" responsive>
                     <thead className="text-primary">
                       <tr>
-                        <th>Client Id</th>
-                        <th>Name</th>
+                        <th>Group Name</th>
                         <th>Platform</th>
                         <th>Region</th>
-                        <th className="text-center">Un Aceptable Conversations</th>
+                        <th className="text-center">Unacceptable Contacts</th>
                       </tr>
                     </thead>
                     <tbody>
                       {
-                        this.state.conversations.map(con => {
+                        this.state.chatGroups.map(con => {
                           return (
                             <tr>
-                            <td>{con.id}</td>
-                            <td>{con.name}</td>
+                            <td><a href="#" onClick={() => this.onGroupSelect(con.name)}>{con.name}</a> </td>
                             <td>{con.platform}</td>
                             <td>{con.region}</td>
-                            <td className="text-center">{con.isAcceptable ? "Acceptable" : "Not Acceptable"}</td>
+                            <td className="text-center">{con.unAcceptableContacts}</td>
                           </tr>
                         )
                         })
@@ -88,6 +151,90 @@ class Tables extends React.Component {
                 </CardBody>
               </Card>
             </Col>
+          { 
+            !this.state.groupSelected ? null :
+            <>
+            (
+              <Col md="12">
+                <Card >
+                  <CardHeader>
+                    <CardTitle tag="h4">Team Details</CardTitle>
+                  </CardHeader>
+                  <CardBody>
+                    <Table className="tablesorter">
+                      <thead className="text-primary">
+                        <tr>
+                          <th>sid</th>
+                          <th>Name</th>
+                          <th>Platform</th>
+                          <th>Region</th>
+                          <th>Total Contacts</th>
+                          <th>Policy Breaks</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {
+                            this.state.users.map(con => {
+                            return (
+                              <tr>
+                              <td><a href="#" onClick={() => this.onUserSelect(con.sid, con.name)}>{con.sid}</a> </td>
+                              <td>{con.name}</td>
+                              <td>{con.platform}</td>
+                              <td>{con.region}</td>
+                              <td>{con.totalContacts.length}</td>
+                              <td className="text-end">{con.totalUnacceptableContacts.length}</td>
+                            </tr>
+                          )
+                          })
+                        }
+                      </tbody>
+                    </Table>
+                  </CardBody>
+                </Card>
+              </Col>
+              {
+                !this.state.userSelected ? null :
+              (<Col md="12">
+                <Card >
+                  <CardHeader>
+                    <CardTitle tag="h4">{`External interactons for ${this.state.selectedUser}`}</CardTitle>
+                  </CardHeader>
+                  <CardBody>
+                    <Table className="tablesorter">
+                      <thead className="text-primary">
+                        <tr>
+                          <th>Client Id</th>
+                          <th>Name</th>
+                          <th>Platform</th>
+                          <th>Region</th>
+                          <th className="text-center">Policy Break</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {
+                          this.state.unAcceptableContacts.map(con => {
+                            return (
+                              <tr>
+                              <td>{con.id}</td>
+                              <td>{con.name}</td>
+                              <td>{con.platform}</td>
+                              <td>{con.region}</td>
+                              <td className="text-center">{con.isAcceptable ? "NO" : "YES"}</td>
+                            </tr>
+                          )
+                          })
+                        }
+                      </tbody>
+                    </Table>
+                  </CardBody>
+                </Card>
+              </Col>)
+              }
+            )
+            </>
+            
+          }
+          
           </Row>
         </div>
       </>
@@ -95,4 +242,4 @@ class Tables extends React.Component {
   }
 }
 
-export default Tables;
+export default connect(mapStateToProps)(ComplianceDashboard);
